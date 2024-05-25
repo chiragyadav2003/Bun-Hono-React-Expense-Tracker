854a043f862545b8570fd23f1f4836c5e6e1f194
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-25 10:47:37 +0530

    adding zod validation

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono';
+import { z } from "zod";
 
-const expensesRoute = new Hono();
 
 type Expenses = {
     id: number,
@@ -8,30 +8,25 @@ type Expenses = {
     amount: number
 }
 
+const createPostSchema = z.object({
+    title: z.string().min(3).max(100),
+    amount: z.number().int().positive()
+})
+
 const fakeExpenses: Expenses[] = [
-    {
-        id: 1,
-        title: "Rent",
-        amount: 1000
-    },
-    {
-        id: 2,
-        title: "Food",
-        amount: 200
-    },
-    {
-        id: 3,
-        title: "Transport",
-        amount: 100
-    }
+    { id: 1, title: "Rent", amount: 1000 },
+    { id: 2, title: "Food", amount: 200 },
+    { id: 3, title: "Transport", amount: 100 }
 ]
+const expensesRoute = new Hono();
 
 expensesRoute.get("/", (c) => {
     return c.json(fakeExpenses);
 })
 
 expensesRoute.post("/", async (c) => {
-    const expense = await c.req.json();
+    const data = await c.req.json();
+    const expense = createPostSchema.parse(data);
     console.log(expense);
     return c.json(expense);
 })
