891e0331e3c2d558c20ca58db95ae084e250094c
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-26 18:54:39 +0530

    add shadcn ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { useState } from "react";
+
 function App() {
 
+  const [totalSpent, setTotalSpent] = useState(0);
+
   return (
-    <div className=" bg-slate-400 h-screen">
-      Bun App
-    </div>
+    <Card className=" w-full max-w-[350px] mx-auto">
+      <CardHeader>
+        <CardTitle>Total Spent</CardTitle>
+        <CardDescription>The amount you've spent</CardDescription>
+      </CardHeader>
+      <CardContent>
+        {totalSpent}
+      </CardContent>
+    </Card>
   )
 }
 
