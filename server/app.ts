10a8dfef920459f17006c90c54393132e0c8a7dd
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-26 22:19:12 +0530

    building the app and static serving app from backend

--- server/app.ts
+++ server/app.ts
@@ -1,17 +1,21 @@
 import { Hono } from 'hono';
 import { logger } from "hono/logger";
+import { serveStatic } from 'hono/bun';
 
 import expensesRoute from './routes/expenses.ts';
 
-const app = new Hono().basePath('/api');
+// const app = new Hono().basePath('/api');
+const app = new Hono();
 app.use('*', logger());
 
-app.get("/", (c) => {
-    return c.text("Hello World");
-})
+//routing expense requests to expense route
+app.route("/api/expenses", expensesRoute);
 
-//adding route
-app.route("/expenses", expensesRoute);
+
+//--------serving static files--------
+// if the requested api route that does not exist on the server, we will serve simple react app
+app.get('*', serveStatic({ root: './frontend/dist' }))
+app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
 
 
 
