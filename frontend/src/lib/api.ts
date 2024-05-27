281b8b94b84d2e84b2e361ddca828c4e5ce10c84
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-27 17:25:15 +0530

    updating import alias for importing files from server directory

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,5 @@
 import { hc } from 'hono/client';
-import { type ApiRoutes } from "../../../server/app.ts";
+import { type ApiRoutes } from "@server/app.ts";
 
 const client = hc<ApiRoutes>('/'); // we add "/" because we have modified it in vite proxy so they are on the same origin
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dir, "./src"),
+      "@server": path.resolve(import.meta.dir, "../server"),
     },
   },
   server: {
