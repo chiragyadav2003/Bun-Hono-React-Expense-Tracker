6d924d1c4d9ab86ce45ec12f7c7c28a99822d95d
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-27 21:35:46 +0530

    adding index.html in tailwind.config to apply dark mode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    'index.html',
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
