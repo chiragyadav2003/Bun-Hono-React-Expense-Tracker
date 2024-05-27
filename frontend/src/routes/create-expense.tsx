857c76f6f7fee45a312e89e8e2cfe43be0cf7337
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-27 23:11:07 +0530

    init - create new expense page

--- frontend/src/routes/create-expense.tsx
+++ frontend/src/routes/create-expense.tsx
@@ -1,4 +1,7 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
 
 export const Route = createFileRoute('/create-expense')({
   component: CreateExpenses,
@@ -6,6 +9,15 @@ export const Route = createFileRoute('/create-expense')({
 
 function CreateExpenses() {
   return (
-    <div>Hello /create-expense!</div>
+    <div className='p-2 flex flex-col items-center'>
+      <h2 className=' text-2xl font-semibold unde'>Create-expense</h2 >
+      <form className='grid w-full max-w-sm items-center gap-y-2 pt-4'>
+        <Label htmlFor="title">Title</Label>
+        <Input type="text" id="title" placeholder='Title' />
+        <Label htmlFor="amount">Title</Label>
+        <Input type="number" id="amount" placeholder='Amount' />
+        <Button type='submit'>Create</Button>
+      </form>
+    </div>
   )
 }
