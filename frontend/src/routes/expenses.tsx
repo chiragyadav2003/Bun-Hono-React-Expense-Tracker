f55c7dc8f130c0263a0d4f6b456b07c359e488c9
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-27 21:50:13 +0530

    JSON.stringify - get all expenses

--- frontend/src/routes/expenses.tsx
+++ frontend/src/routes/expenses.tsx
@@ -1,11 +1,33 @@
+import { api } from '@/lib/api';
+import { useQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/expenses')({
   component: Expenses,
 })
 
+async function getAllExpenses() {
+  const res = await api.expenses.$get();
+  if (!res.ok) {
+    throw new Error("Server error");
+  }
+  const data = await res.json();
+  return data;
+}
+
 function Expenses() {
+  const { isPending, data, error } = useQuery({
+    queryKey: ['get-all-expenses'],
+    queryFn: getAllExpenses,
+  })
+
+  if (error) return "An error has occured : " + error.message;
+
   return (
-    <div>Hello expense!</div>
+    <div>
+      <pre>
+        {isPending ? "..." : JSON.stringify(data, null, 2)}
+      </pre>
+    </div>
   )
 }
