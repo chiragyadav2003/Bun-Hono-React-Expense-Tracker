9d3f0bc3397deba0ef7d0c953593c75b42572d3a
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-30 00:13:14 +0530

    add sample profile section

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -8,17 +8,20 @@ export const Route = createRootRoute({
 function NavBar() {
     return (
         <div className="p-2 flex gap-2">
-            <Link to="/" className="[&.active]:font-bold">
-                Home
-            </Link>{' '}
             <Link to="/about" className="[&.active]:font-bold">
                 About
             </Link>
+            <Link to="/" className="[&.active]:font-bold">
+                Home
+            </Link>{' '}
             <Link to="/expenses" className="[&.active]:font-bold">
                 Expenses
             </Link>
             <Link to="/create-expense" className="[&.active]:font-bold">
                 Create
+            </Link>
+            <Link to="/profile" className="[&.active]:font-bold">
+                Profile
             </Link>
         </div>
     )
---
+++ frontend/src/routes/profile.tsx
@@ -0,0 +1,23 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { useQuery } from "@tanstack/react-query";
+import { userQueryOptions } from "@/lib/api";
+
+export const Route = createFileRoute('/profile')({
+    component: Profile,
+})
+
+
+
+function Profile() {
+    const { isPending, data, error } = useQuery(userQueryOptions)
+    if (isPending) return "Loading...";
+    if (error) return "not logged in";
+    return (
+        <div>
+            <h1>Profile</h1>
+            <p>
+                Hello, {data.user.given_name} {data.user.family_name}
+            </p>
+        </div>
+    )
+}
