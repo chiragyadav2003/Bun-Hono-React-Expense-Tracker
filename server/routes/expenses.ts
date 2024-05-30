30db62315224488f503bc48d4aed04f0b1b6947f
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-30 21:43:28 +0530

    feat: Add pagination and ordering to expenses route
    This commit modifies the expenses route in the `expenses.ts` file to include pagination and ordering for the retrieved expenses. The expenses are now ordered by descending creation date and limited to 100 results. This change improves the user experience by providing a more manageable and organized list of expenses.

--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -6,8 +6,7 @@ import { getUser } from "../kinde.ts";
 
 import { expenses as expensesTable } from "../db/schema/expenses.ts";
 import { db } from "../db/index.ts";
-import { eq } from 'drizzle-orm';
-
+import { eq, desc, sum, and } from 'drizzle-orm';
 
 
 const ExpenseSchema = z.object({
@@ -21,60 +20,73 @@ type Expenses = z.infer<typeof ExpenseSchema>
 //in createPostSchema we do not require id so omit it
 const createPostSchema = ExpenseSchema.omit({ id: true })
 
-const fakeExpenses: Expenses[] = [
-    { id: 1, title: "Rent", amount: "1000" },
-    { id: 2, title: "Food", amount: "200" },
-    { id: 3, title: "Transport", amount: "100" }
-]
-
-
 const expensesRoute = new Hono()
+    // get all expenses information
     .get("/", getUser, async (c) => {
         const user = c.var.user;
-        const expenses = await db.select().from(expensesTable).where(eq(expensesTable.userId, user.id))
+        const expenses = await db
+            .select()
+            .from(expensesTable)
+            .where(eq(expensesTable.userId, user.id))
+            .orderBy(desc(expensesTable.createdAt))
+            .limit(100) //added pagination
         return c.json({ expenses: expenses });
     })
     //here zValidator will validate incoming request 'json' data as per createPostSchema validation
     .post("/", getUser, zValidator('json', createPostSchema), async (c) => {
         const expense = await c.req.valid('json');
         const user = c.var.user;
-
         const res = await db.insert(expensesTable).values({
             ...expense,
             userId: user.id,
-
         }).returning()
-
-        fakeExpenses.push({ ...expense, id: fakeExpenses.length + 1 })
-        return c.json({ msg: "Expense ceated successfully", expense }, 201)
+        return c.json({ msg: "Expense ceated successfully", res }, 201)
     })
     // regex {[0-9]+} ensures that id entered is integer
     .get("/:id{[0-9]+}", getUser, async (c) => {
         //id is of type string so we need to change it to number
         const id = Number.parseInt(c.req.param('id'));
-        const expense = fakeExpenses.find(expense => expense.id === id);
+        const user = c.var.user;
+        const expense = await db
+            .select()
+            .from(expensesTable)
+            .where(and(eq(expensesTable.userId, user.id), eq(expensesTable.id, id)))
+            .orderBy(desc(expensesTable.createdAt))
+            .then(res => res[0]);
+
         if (!expense) {
             return c.notFound()
         }
+
         return c.json({ expense }, 201)
     })
     //total expense
-    .get('total-spent', getUser, (c) => {
-        const total = fakeExpenses.reduce((acc, expense) => acc + +expense.amount, 0)
-        return c.json({ total })
+    .get('total-spent', getUser, async (c) => {
+        const user = c.var.user;
+        const result = await db
+            .select({ total: sum(expensesTable.amount) })
+            .from(expensesTable)
+            .where(eq(expensesTable.userId, user.id))
+            .limit(1)
+            .then(res => res[0]);
+        return c.json(result)
     })
     .delete("/:id{[0-9]+}", getUser, async (c) => {
         //id is of type string so we need to change it to number
         const id = Number.parseInt(c.req.param('id'));
-        const expenseIndex = fakeExpenses.findIndex(expense => expense.id === id);
-        if (expenseIndex == -1) {
+        const user = c.var.user;
+
+        const expense = await db
+            .delete(expensesTable)
+            .where(and(eq(expensesTable.userId, user.id), eq(expensesTable.id, id)))
+            .returning()
+            .then(res => res[0])
+
+        if (!expense) {
             return c.notFound()
         }
 
-        //1st parameter in index , 2nd parameter means remove one item only
-        const deleteExpense = fakeExpenses.splice(expenseIndex, 1)
-
-        return c.json({ msg: "expense deleted successfully", deleteExpense }, 202)
+        return c.json({ msg: "expense deleted successfully", deletedExpense: expense }, 202)
     })
 
 
